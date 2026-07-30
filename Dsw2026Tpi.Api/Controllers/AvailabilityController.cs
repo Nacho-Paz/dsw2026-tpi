20fede7b80af76b0e23e208b7eb97de53eb73f7a
Milena Coronel <coronelanamilena@gmail.com>
2026-07-30 16:42:25 -0300

    Cambio en la ruta para disponibilidad

--- Dsw2026Tpi.Api/Controllers/AvailabilityController.cs
+++ Dsw2026Tpi.Api/Controllers/AvailabilityController.cs
@@ -1,6 +1,32 @@
-﻿namespace Dsw2026Tpi.Api.Controllers
+﻿using Dsw2026Tpi.Application.Dtos;
+using Dsw2026Tpi.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+namespace Dsw2026Tpi.Api.Controllers
 {
-    public class AvailabilityController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AvailabilityController : ControllerBase
     {
+        private readonly IAvailabilityService _availabilityService;
+        public AvailabilityController(IAvailabilityService availabilityService)
+        {
+            _availabilityService = availabilityService;
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> CreateAvailabilities([FromBody] AvailabilityModel.Request request)
+        {
+            await _availabilityService.CreateAvailabilitiesAsync(request);
+            return Ok(new AvailabilityModel.Response("Disponibilidades creadas con éxito."));
+        }
+        
+        
+        [HttpPut]
+        public async Task<IActionResult> UpdateAvailabilities([FromBody] AvailabilityModel.Request request)
+        {
+            await _availabilityService.UpdateAvailabilitiesAsync(request);
+            return Ok(new AvailabilityModel.Response("Disponibilidades actualizadas con éxito."));
+        }
+        }
     }
-}
+
