53e49fd24c68d6a5d5205452584d37ba224fd98f
Valentina Luna <lourdes.lunaacosta@alu.frt.utn.edu.ar>
2026-08-01 00:35:06 -0300

    Agrego DTO de creacion

---
+++ Dsw2026Tpi.Application/Dtos/DoctorCreateModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Dsw2026Tpi.Application.Dtos
+{
+    public class DoctorCreateModel
+    {
+        [Required(ErrorMessage = "El nombre del médico es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El nombre no puede superar los 100 caracteres.")]
+        public string Name { get; set; } = string.Empty;
+        [Required(ErrorMessage = "La matrícula es obligatoria.")]
+        public string LicenseNumber { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Debe asociar una especialidad.")]
+        public Guid SpecialtyId { get; set; }
+    }
+}
