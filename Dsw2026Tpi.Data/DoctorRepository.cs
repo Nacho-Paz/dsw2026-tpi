9f3b438610a6d92154d071023dcd97e97e3eae5f
Valentina Luna <lourdes.lunaacosta@alu.frt.utn.edu.ar>
2026-08-01 00:26:14 -0300

    Interfaz creada

---
+++ Dsw2026Tpi.Data/DoctorRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Tpi.Data
+{
+    internal class DoctorRepository
+    {
+    }
+}
+
+//DEJO CREADA PARA QUE AGUS DESARROLLE
---
+++ Dsw2026Tpi.Domain/Interfaces/IDoctorRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Tpi.Domain.Interfaces
+{
+    internal class IDoctorRepository
+    {
+    }
+}
+
+//DEJO YA CREADO PARA QUE AGUS DESARROLLE
