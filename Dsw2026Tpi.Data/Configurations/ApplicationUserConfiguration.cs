9451995078f98f26820956f3c6900b145f93890c
NachoPaz <82242852+Nacho-Paz@users.noreply.github.com>
2026-08-03 16:38:43 -0300

    Create ApplicationUserConfiguration and config OnModelCreating in AuthenticationDbContext

---
+++ Dsw2026Tpi.Data/Configurations/ApplicationUserConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Dsw2026Tpi.Data.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dsw2026Tpi.Data.Configurations;
+
+public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.ToTable("ApplicationUsers");
+
+        builder.Property(x => x.CreatedAt).IsRequired();
+
+        builder.Property(x => x.UpdatedAt).IsRequired();
+    }
+}
