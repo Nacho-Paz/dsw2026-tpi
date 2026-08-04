671766057925c8988ef6b8613fadcfab8ece5988
NachoPaz <82242852+Nacho-Paz@users.noreply.github.com>
2026-08-04 08:45:15 -0300

    Correct order in exceptions

--- Dsw2026Tpi.CrossCutting/Exceptions/AuthenticationException.cs
+++ Dsw2026Tpi.CrossCutting/Exceptions/AuthenticationException.cs
@@ -8,9 +8,8 @@ namespace Dsw2026Tpi.CrossCutting.Exceptions;
 public class AuthenticationException : AppException
 {
     public AuthenticationException()
-        : base(
-            ErrorCodes.AUTHENTICATION_FAILED,
-            nameof(ErrorCodes.AUTHENTICATION_FAILED))
+        : base(nameof(ErrorCodes.AUTHENTICATION_FAILED),
+            ErrorCodes.AUTHENTICATION_FAILED)
     {
     }
 }
--- Dsw2026Tpi.Data/Identity/ApplicationUser.cs
+++ Dsw2026Tpi.Data/Identity/ApplicationUser.cs
@@ -5,7 +5,7 @@ namespace Dsw2026Tpi.Data.Identity;
 public class ApplicationUser: IdentityUser
 {
     public bool IsActive { get; set; } = true;
-    public string? Dni { get; set; }
+    public string? Dni { get; set; } //TODO: Ver que onda esto, no me conviene que este en patient?
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 }
