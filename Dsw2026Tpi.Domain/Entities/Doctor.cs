e15474a9549d37afbc4413c9d1042e2e8910f65b
NachoPaz <82242852+Nacho-Paz@users.noreply.github.com>
2026-09-08 17:49:26 -0300

    Refactor Doctor

--- Dsw2026Tpi.Domain/Entities/Doctor.cs
+++ Dsw2026Tpi.Domain/Entities/Doctor.cs
@@ -1,15 +1,13 @@
 ﻿namespace Dsw2026Tpi.Domain.Entities;
 
-public class Doctor: EntityBase
+public class Doctor : EntityBase
 {
     public string Name { get; private set; } = string.Empty;
     public string LicenseNumber { get; private set; } = string.Empty;
     public bool IsActive { get; private set; }
     public Guid SpecialityId { get; set; }
     public Specialty Speciality { get; private set; } = null!;
-
-    public ICollection<AvailabilityRule> AvailabilityRules
-    { get; private set; } = new List<AvailabilityRule>();
+    public ICollection<AvailabilityRule> AvailabilityRules { get; private set; } = new List<AvailabilityRule>();
 
     #region Constructor for EF
 #pragma warning disable CS8618
@@ -37,5 +35,5 @@ public class Doctor: EntityBase
     {
         IsActive = false;
     }
-   
+
 }
